456f98affeaf43df3a388c0fc1dcd6d23351aa11
Harsh Ahuja <harshahuja@Harshs-MacBook-Air.local>
2025-02-16 22:59:54 +0530

    base-url-update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 // export const BASE_URL =
 //   location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
+// export const BASE_URL =
+//   "http://localhost:3001";
+
 export const BASE_URL =
-  "http://localhost:3001";
+ "/api";
